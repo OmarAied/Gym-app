d0bf3ab038612c940bfa3d56f13910899a6ad3d0
Omar Aied <omaraied007@gmail.com>
2025-02-06 15:04:33 +0000

    Navigation bar

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint 
+from flask import Blueprint, render_template
 #Stores standard routes for the website
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home(): #Whenever / is typed for url, takes to home
-    return "<h1>Test</h1>"
+    return render_template("home.html") #Renders home.html
 
